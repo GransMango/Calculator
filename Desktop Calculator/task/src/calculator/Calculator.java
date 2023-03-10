50179e5ad0f88c3a9033ef8240946df8a2b9a2b2
Daniel <GransMango@github.com>
2023-03-10 17:55:10 +0100

    Added a Calculation class which replaces solve and adds multiple operators

--- Desktop Calculator/task/src/calculator/Calculator.java
+++ Desktop Calculator/task/src/calculator/Calculator.java
@@ -1,13 +1,16 @@
 package calculator;
 
 import javax.swing.*;
-import java.util.ArrayList;
+import java.awt.*;
+import java.util.*;
 import java.util.List;
 
 public class Calculator extends JFrame {
-    private List<JButton> numberButtons;
-    private List<JButton> operandButtons;
-    private JTextField equationTextField;
+    private final List<JButton> numberButtons;
+    private final List<JButton> operandButtons;
+    private final List<JButton> functionalButtons;
+    private final JLabel resultLabel;
+    private final JLabel equationLabel;
     public Calculator() {
         super("Calculator");
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
@@ -17,17 +20,21 @@ public class Calculator extends JFrame {
         setLocationRelativeTo(null);
         numberButtons = createNumbers();
         operandButtons = createOperands();
-        equationTextField = createTextField();
+        resultLabel = createResultField();
+        equationLabel = createEquationField();
+        functionalButtons = createFunctionalButtons();
         numberButtons.forEach(this::add);
         operandButtons.forEach(this::add);
-        add(equationTextField);
+        functionalButtons.forEach(this::add);
+        add(resultLabel);
+        add(equationLabel);
         createAction();
     }
 
     private List<JButton> createNumbers() {
         List<JButton> buttonList = new ArrayList<>();
         int x = 150;
-        int y = 80;
+        int y = 120;
         int width = 50;
         int height = 50;
         String[] numberNames = {"Nine", "Eight", "Seven", "Six", "Five", "Four", "Three", "Two", "One", "Zero"};
@@ -42,7 +49,7 @@ public class Calculator extends JFrame {
                 x = 150;
                 y += 60;
             }
-            if (y > 210) {
+            if (y > 250) {
                 x = 90;
             }
         }
@@ -52,7 +59,7 @@ public class Calculator extends JFrame {
     private List<JButton> createOperands() {
         List<JButton> buttonList = new ArrayList<>();
         int x = 210;
-        int y = 80;
+        int y = 120;
         int width = 50;
         int height = 50;
         String[] operands = {"/", "x", "+", "-"};
@@ -65,80 +72,95 @@ public class Calculator extends JFrame {
             buttonList.add(button);
             y += 60;
         }
+
         JButton button = new JButton();
         button.setName("Equals");
         button.setText("=");
         button.setBounds(x-60, y-60, width, height);
         buttonList.add(button);
+
+        JButton dotButton = new JButton();
+        dotButton.setName("Dot");
+        dotButton.setText(".");
+        dotButton.setBounds(x-180, y-60, width, height);
+        buttonList.add(dotButton);
         return buttonList;
     }
 
-    private JTextField createTextField() {
-        JTextField textField = new JTextField();
-        textField.setName("EquationTextField");
+    private List<JButton> createFunctionalButtons() {
+        List<JButton> functionalButtonList = new ArrayList<>();
+
+        int x = 210;
+        int y = 60;
+        int width = 50;
+        int height = 50;
+
+        JButton deleteButton = new JButton();
+        deleteButton.setName("Delete");
+        deleteButton.setText("Del");
+        deleteButton.setBounds(x-5, y, width + 5, height);
+        functionalButtonList.add(deleteButton);
+
+        JButton clearButton = new JButton();
+        clearButton.setName("Clear");
+        clearButton.setText("C");
+        clearButton.setBounds(x-65, y, width + 5, height);
+        functionalButtonList.add(clearButton);
+
+        deleteButton.addActionListener(e -> {
+            if (resultLabel.getText().length() > 0) {
+                resultLabel.setText(resultLabel.getText().substring(0, resultLabel.getText().length() - 1));
+            }
+        });
+
+        clearButton.addActionListener(e -> {
+            equationLabel.setText("");
+            resultLabel.setText("");
+        });
+        return functionalButtonList;
+    }
+
+    private JLabel createResultField() {
+        JLabel textField = new JLabel();
+        textField.setName("ResultLabel");
+        textField.setFont(new Font("DecoType Naskh", Font.BOLD, 20));
         textField.setBounds(25, 10, 240, 30);
+        return textField;
+    }
+
+    private JLabel createEquationField() {
+        JLabel textField = new JLabel();
+        textField.setName("EquationLabel");
+        textField.setFont(new Font("Serif Italic", Font.BOLD, 12));
+        textField.setForeground(Color.GRAY);
+        textField.setBounds(25, 30, 240, 30);
         return textField;
     }
 
     private void createAction() {
         for (JButton button : numberButtons) {
             button.addActionListener(e -> {
-                String content = equationTextField.getText();
+                String content = resultLabel.getText();
                 content += button.getText();
-                equationTextField.setText(content);
+                resultLabel.setText(content);
             });
         }
         for (JButton button : operandButtons) {
             if (!button.getActionCommand().equals("=")) {
                 button.addActionListener(e -> {
-                    String content = equationTextField.getText();
+                    String content = resultLabel.getText();
                     content += button.getText();
-                    equationTextField.setText(content);
+                    resultLabel.setText(content);
                 });
             } else {
                 button.addActionListener(e -> {
-                    String content = equationTextField.getText();
-                    content += button.getText();
-                    content += solve();
-                    equationTextField.setText(content);
+                    String content = resultLabel.getText();
+                    equationLabel.setText(content);
+                    content = String.valueOf(Calculation.evaluate(content));
+                    resultLabel.setText(content);
                 });
             }
         }
     }
 
-    private String solve() {
-        List<String> operands = new ArrayList<>();
-        operandButtons.forEach(e -> operands.add(e.getActionCommand()));
-        int operand = -1;
-        String equation_raw = equationTextField.getText();
-        if (!equation_raw.isEmpty()) {
-            for (int i = 0; i < operands.size()-1; i++) {
-                if (equation_raw.contains(operands.get(i))) {
-                    operand = i;
-                    System.out.println(operands.get(i));
-                }
-            }
-        }
-        if (operand == -1) {
-            System.out.println("test");
-            return "-1";
-        }
-        String[] equation_split;
-        if (operand == 2) {
-            equation_split = equation_raw.split("\\+");
-        } else if (operand == 3) {
-            equation_split = equation_raw.split("\\-");
-        } else {
-            equation_split = equation_raw.split(operands.get(operand));
-        }
-        if (operand == 0) {
-            return String.valueOf(Integer.parseInt(equation_split[0]) / Integer.parseInt(equation_split[1]));
-        } else if (operand == 1) {
-            return String.valueOf(Integer.parseInt(equation_split[0]) * Integer.parseInt(equation_split[1]));
-        } else if (operand == 2) {
-            return String.valueOf(Integer.parseInt(equation_split[0]) + Integer.parseInt(equation_split[1]));
-        } else {
-            return String.valueOf(Integer.parseInt(equation_split[0]) - Integer.parseInt(equation_split[1]));
-        }
-    }
 }
